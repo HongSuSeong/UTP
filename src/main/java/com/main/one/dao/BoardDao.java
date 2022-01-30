bc4400634282e774803516761d1874ad64b41a22
ollzl <ollzl@192.168.0.107>
2022-01-30 22:34:15 +0900

    commit

--- src/main/java/com/main/one/dao/BoardDao.java
+++ src/main/java/com/main/one/dao/BoardDao.java
@@ -1,10 +1,17 @@
 package com.main.one.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Repository;
+
+import com.main.one.vo.boardVO;
 
 @Repository
 public interface BoardDao {
 
-	public Object getBoardList();
+	public ArrayList<HashMap<String,Object>> getBoardList(HashMap<String,Object>map);
+
+	public Object newWrite(boardVO board);
 
 }
--- src/main/java/com/main/one/service/BoardService.java
+++ src/main/java/com/main/one/service/BoardService.java
@@ -1,17 +1,25 @@
 package com.main.one.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.main.one.dao.BoardDao;
+import com.main.one.vo.boardVO;
 
 @Service
 public class BoardService {
 	
 	@Autowired BoardDao boardDao;
 
-	public Object getBoardList() {
-		return boardDao.getBoardList();
+	public ArrayList<HashMap<String,Object>> getBoardList(HashMap<String,Object> map) {
+		return boardDao.getBoardList(map);
+	}
+
+	public void newWrite(boardVO board) {
+		boardDao.newWrite(board);
 	}
 
 }
